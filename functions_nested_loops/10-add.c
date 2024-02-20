bc89a233535b14f846708ead93dff1898ded690c
HolbieEnjoyer <8698@holbertonstudents.com>
2024-02-20 01:13:26 -0800

    Next task

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*add - returns the sign of a number
+*@a: number to be checked
+*@b: number to be checked
+*Return: -1, 1 or 0
+*/
+int add(int a, int b)
+{
+return (a + b);
+}

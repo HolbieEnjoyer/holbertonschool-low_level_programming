d09dbf5d4261adeeeaf2db8670cc8a653111d181
HolbieEnjoyer <8698@holbertonstudents.com>
2024-02-20 00:43:39 -0800

    Next task

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+*print_num - Fuck Betty
+*@t: frick betty
+*/
+void print_num(int t)
+{
+char first;
+
+first = t < 10 ? '\0' : ('0' + t / 10);
+_putchar(first);
+_putchar('0' + t % 10);
+}
+
+/**
+*times_table - Fuck Betty
+*/
+void times_table(void)
+{
+int i;
+int j;
+
+for (j = 0; j < 9; j++)
+{
+for (i = 0; i < 9; i++)
+{
+print_num(i * j);
+_putchar(',');
+_putchar(' ');
+_putchar('\n');
+}
+}
+}

31e169f9f08399e558c56bfab14b9fe8f929f3d1
HolbieEnjoyer <8698@holbertonstudents.com>
2024-02-20 01:27:38 -0800

    Next task

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
 /**
+*sign - returns the sign of a number
+*@n: number to be checked
+*Return: -1, 1 or 0
+*/
+int sign(int n)
+{
+return n < 0 ? n * -1 : n;
+}
+
+/**
 *print_num - Fuck Betty
 *@t: frick betty
 */
@@ -19,9 +29,18 @@ _putchar('0' + t % 10);
 */
 void print_to_98(int n)
 {
-int i;
+if (n == 98)
+{
+print_num(98);
+_putchar('\n');
+return;
+}
 
-for (i = n; i < 99; i++)
+int i;
+int inc;
+
+inc = sign(98 - n);
+for (i = n; i != 98; i += inc)
 {
 print_num(n);
 if (i == 98)

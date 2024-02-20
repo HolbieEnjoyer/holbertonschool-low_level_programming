1172a5a4ddd0b3650f293405956780e3bd6653bb
HolbieEnjoyer <8698@holbertonstudents.com>
2024-02-19 23:25:10 -0800

    Next task

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*_abs - returns the absolute value of a number
+*@n: number to be checked
+*Return: the absolute value
+*/
+int _abs(int n)
+{
+return (n < 0 ? n * -1 : n);
+}

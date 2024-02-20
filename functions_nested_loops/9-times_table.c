4b5acf7154081a87b46d6c05035a9674a0685a34
HolbieEnjoyer <8698@holbertonstudents.com>
2024-02-20 02:53:56 -0800

    Next task

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+*sign - returns the sign of a number
+*@n: number to be checked
+*Return: -1, 1 or 0
+*/
+int sign(int n)
+{
+if (n < 0)
+	return (-1);
+else if (n > 0)
+	return (1);
+else
+	return (0);
+}
+
+/**
+*abs - returns the sign of a number
+*@n: number to be checked
+*Return: -1, 1 or 0
+*/
+int abs(int n)
+{
+return (n < 0 ? n * -1 : n);
+}
+
+/**
 *print_num - Fuck Betty
 *@t: frick betty
 */

20c9514139e04b26b78cee808249dc10e156ff58
HolbieEnjoyer <8698@holbertonstudents.com>
2024-02-28 09:40:58 +0000

    Next task

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,56 +1,21 @@
 #include "main.h"
 
 /**
-*sign - returns the sign of a number
-*@n: number to be checked
-*Return: -1, 1 or 0
-*/
-int sign(int n)
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
 {
-if (n < 0)
-	return (-1);
-else if (n > 0)
-	return (1);
-else
-	return (0);
-}
+	unsigned int num = n;
 
-/**
-*abs - returns the sign of a number
-*@n: number to be checked
-*Return: -1, 1 or 0
-*/
-int abs(int n)
-{
-return (n < 0 ? n * -1 : n);
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-/**
-*print_num - Fuck Betty
-*@t: frick betty
-*/
-void print_number(int t)
-{
-int num;
-int dec;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-if (t < 0)
-	_putchar('-');
-t = abs(t);
-
-num = t;
-dec = 1;
-
-while (num >= 10)
-{
-dec *= 10;
-num /= 10;
-}
-
-while (dec != 1)
-{
-_putchar('0' + (t / dec) % 10);
-dec /= 10;
-}
-_putchar('0' + t % 10);
+	_putchar((num % 10) + '0');
 }

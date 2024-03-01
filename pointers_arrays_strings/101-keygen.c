4a2c5e5ba2ec12e7fc298acc2dcfb9681d53e727
HolbieEnjoyer <8698@holbertonstudents.com>
2024-03-01 15:16:16 +0000

    Next task

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -16,12 +16,13 @@ int rand_char() {
 int main() {
     
     int sum = 0;
+    int leftover = 0;
     while (1) {
         int ch = rand_char();
         sum += ch;
         if (sum > 2600) {
 	    putchar(ch);
-	    int leftover = 2772 - sum;
+	    leftover = 2772 - sum;
 	    putchar(leftover / 2);
             putchar(leftover - leftover / 2);
             break;

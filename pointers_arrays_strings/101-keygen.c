91400f4c3efb25fa6a52a93eb787b5d7ee0dcfc9
HolbieEnjoyer <8698@holbertonstudents.com>
2024-03-01 15:14:28 +0000

    Next task

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -21,9 +21,7 @@ int main() {
         sum += ch;
         if (sum > 2600) {
 	    putchar(ch);
-	    int leftover;
-
-	    leftover = 2772 - sum;
+	    int leftover = 2772 - sum;
 	    putchar(leftover / 2);
             putchar(leftover - leftover / 2);
             break;

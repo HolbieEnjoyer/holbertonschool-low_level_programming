7cf9288dc1aab2bc719aeec0b50dc311adfa378b
HolbieEnjoyer <8698@holbertonstudents.com>
2024-03-13 09:00:12 +0000

    Next task

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: variable
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+		p++;
+	return (p - s);
+}
+
 /**
  * _strdup - fills memory
  * @str: str
@@ -16,7 +32,7 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	size = strlen(str);
+	size = _strlen(str);
 	arr = (char *)malloc(sizeof(char) * size + 1);
 	if (arr == NULL)
 	{

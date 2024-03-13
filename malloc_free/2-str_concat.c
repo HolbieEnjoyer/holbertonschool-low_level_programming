6d0b29f332b1a870568aee4f06b65276c052cd28
HolbieEnjoyer <8698@holbertonstudents.com>
2024-03-13 09:12:23 +0000

    Next task

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: variable
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+		p++;
+	return (p - s);
+}
+
 /**
  * str_concat - concatenates two strings
  * @s1: str

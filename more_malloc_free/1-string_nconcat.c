99fa4086b4b96a3976dad928fe371a05418f0736
HolbieEnjoyer <8698@holbertonstudents.com>
2024-03-14 10:17:53 +0000

    Next task

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: variable
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+		p++;
+	return (p - s);
+}
+
 /**
  * string_nconcat - first and n elements of 2nd str
  * @s1: first string
@@ -16,11 +32,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		l1 = 0;
 	else
-		l1 = strlen(s1);
+		l1 = _strlen(s1);
 	if (s2 == NULL)
 		l2 = 0;
 	else
-		l2 = strlen(s2);
+		l2 = _strlen(s2);
 	if (n >= l2)
 		n = l2;
 	l = l1 + n;

823a53727f2eb551442bc72452a3aac73ef19bbd
HolbieEnjoyer <8698@holbertonstudents.com>
2024-04-01 10:47:38 +0000

    Next task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,10 +4,9 @@
 #include "lists.h"
 
 /**
- * _strlen - Returns the length of a string
- *
- * @s: The string to check
- * Return: Always 0.
+ * _strlen - Returns the length of a string.
+ * @s: The string to check.
+ * Return: Length of the string.
  */
 int _strlen(const char *s) {
 	int i;
@@ -16,7 +15,7 @@ int _strlen(const char *s) {
 	while (s[i] != 0)
 		i++;
 	
-	return i;
+	return (i);
 }
 
 /**
